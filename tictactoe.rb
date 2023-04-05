9506eacdfaf6d7212f3bf09f6f7e0fa4bdfe78de
Daniel <118195299+ElNachoMojado@users.noreply.github.com>
2023-04-05 07:31:21 +0100

    Add get_move method to Game class to get the player's next move/Add update method to Board class to update the board with the player's move

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,9 +1,12 @@
 class Game
-  attr_accessor :board
+  attr_accessor :board, :p_move, :player
 
   def initialize
     @board = Board.new
-    #@player = "X"
+    @player = "X"
+    @bot = "O"
+    @p_move = []
+    @b_move = []
   end
 
 #Starts the game loop
@@ -11,12 +14,27 @@ class Game
   end
 
 #Gets the player move
-  def get_move(column, row)
-    
+  def get_move
+    number = 0
+    puts "Where are you placing your next mark?"
+    2.times do |index|
+      if index == 0 
+        puts "Enter row number"
+      else 
+        puts "Enter column number"
+      end
+      loop do
+        number = gets.to_i
+        break if number > 0 && number <=3
+      end
+      @p_move[index] = number - 1
+    end  
+    p @p_move
   end
 
 #Makes a a random move for the opponent
   def bot_move
+
   end
 
 #Checks if any endgame conditions have been met (win, lose, tie)
@@ -25,6 +43,7 @@ class Game
 end
 
 class Board
+
   attr_accessor :board
 
   def initialize
@@ -33,17 +52,22 @@ class Board
 
 #Prints the current state of the board
   def display
-    for row in board do
-        print row
-        puts ""
+    for row in @board do
+        p row
     end
   end
 
 #Updates the board with the players' move
-  def update
+  def update(instance_of_Game)
+    @board[instance_of_Game.p_move[0]][instance_of_Game.p_move[1]] = instance_of_Game.player
+    instance_of_Game.board = @board
   end
 end
 
 a = Game.new
+b = Board.new
 
+a.board.display
+a.get_move
+b.update(a)
 a.board.display
